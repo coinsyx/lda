809effb0d3a68ba6680e7c68629db4e8fb29906e
coinsyx <coinsyx@163.com>
2014-08-20 19:10:52 +0800

    query extend version 1

--- model.cpp
+++ model.cpp
@@ -1,13 +1,31 @@
 #include "model.h"
 
 
+struct cmper {
+    bool operator()(const pair<string, double>& lhs, const pair<string, double>& rhs)
+    {
+        return lhs.second > rhs.second;
+    }
+};
+
+ostream& operator<<(ostream& out, vector<pair<string, double> >& v)
+{
+    for (size_t i=0; i<v.size(); i++)
+        out<<v[i].first<<":"<<v[i].second<<endl;
+    return out;
+}
+
 int main(int argc, char *argv[])
 {
+    google::InitGoogleLogging(argv[0]);
+    FLAGS_logtostderr = 1;
+
     if (argc < 2)
     {
         cout<<"Usage: "<<argv[0]<<"model_file alpha input_query"<<endl;
         return 0;
     }
+
     string model_file = argv[1];
     double alpha = boost::lexical_cast<double>(argv[2]);
     string query = argv[3];
@@ -15,15 +33,14 @@ int main(int argc, char *argv[])
     vector<string> tokens;
     boost::split(tokens, query, boost::is_any_of(" "));
 
-    Model model(model_file);
-    Sampler sampler(&model, alpha, 0.0);
-    LdaInfer infer(&sampler);
-
-    Document doc;
-    infer.Infer(tokens, 20, &doc);
-    
-    for (size_t i = 0; i < doc._document.size(); ++i)
-        cout<<doc._document[i]<<":"<<doc._topic[i]<<endl;
-
+    //Sampler sampler(&model, alpha, 0.0);
+    //LdaInfer infer(model_file, alpha, 0.0);
+    LDAQueryExtend lda_query_extender(model_file, alpha, 0.0); 
+    unordered_map<string, double> extended_query;
+    lda_query_extender.ExtendQuery(tokens, &extended_query); 
+    cout<<"------ extended query --------"<<endl;    
+    vector<pair<string, double> > rst(extended_query.begin(), extended_query.end());
+    sort(rst.begin(), rst.end(), cmper()); 
+    cout<<rst<<endl;
     return 0;
 }
--- model.h
+++ model.h
@@ -11,6 +11,9 @@
 #include <algorithm>
 #include <functional>
 #include <ext/functional>
+#include <glog/logging.h>
+#include <stdio.h>
+#include <map>
 using namespace std;
 using namespace __gnu_cxx;
 using tr1::unordered_map;
@@ -23,14 +26,32 @@ struct Document
     vector<int> _document;
     vector<int> _topic;
     TopicCountDist _topic_dist;    
+    string document_output_string()
+    {
+        string output;
+        char buf[100];
+        for (size_t i = 0; i < _document.size(); ++i)
+        {
+            snprintf(buf, sizeof(buf), "%d:%d ", _document[i], _topic[i]);
+            output += buf;
+        }
+        return output;
+    }
 };
 
+void print_topic_count_dist(const TopicCountDist& topic_count_dist)
+{
+    for (TopicCountDist::const_iterator iter = topic_count_dist.begin();
+         iter != topic_count_dist.end();
+         ++iter)
+        cout<<"topic_id:"<<iter->first<<" : "<<iter->second<<endl;
+}
 
 class Model {
 public:
     typedef unordered_map<int, TopicCountDist* >  WordTopicCountDist;
-    // word -> wordid
-    typedef unordered_map<string, int> WordDict;
+    typedef unordered_map<string, int> Word2IDDict;
+    typedef unordered_map<int, string> ID2WordDict;
     
     Model (const string& model_file)
     {
@@ -54,11 +75,13 @@ public:
                 string word = tokens[0];
                 double count = boost::lexical_cast<double>(tokens[1]);
 
-                if (_word_dict.find(word) == _word_dict.end())
+                if (_word2id_dict.find(word) == _word2id_dict.end())
                 {
-                    _word_dict[word] = _word_dict.size();
+                    int size = _word2id_dict.size();
+                    _word2id_dict[word] = size;
+                    _id2word_dict[size] = word;
                 } 
-                int word_id = _word_dict[word];
+                int word_id = _word2id_dict[word];
                 if (_wor2top.find(word_id) == _wor2top.end())
                 {
                     _wor2top[word_id] = new TopicCountDist();
@@ -70,6 +93,8 @@ public:
 
             _top_total[topic_id] = topic_total_count;
         }
+        LOG(INFO)<<"Load Model over: num_topic="<<_top_total.size()
+                 <<" num_vocal="<<_word2id_dict.size()<<endl;
     }
 
     inline const TopicCountDist* GetWordTopicCountDist (int word_id)
@@ -86,53 +111,94 @@ public:
     {
         return _top_total.size();
     }
+
     inline int GetVocalNum()
     {
-        return _word_dict.size();
+        return _word2id_dict.size();
     }
 
-    inline WordDict* GetWordDict()
+    inline Word2IDDict* GetWord2IDDict()
     {
-        return &_word_dict;
+        return &_word2id_dict;
+    }
+
+    inline ID2WordDict* GetID2WordDict()
+    {
+        return &_id2word_dict;
     }
 
     ~Model()
-    {    }
+    {    
+        for (WordTopicCountDist::iterator iter = _wor2top.begin();
+             iter != _wor2top.end();
+             ++iter)
+        {
+            delete iter->second;
+            iter->second = NULL;
+        }
+    }
 
 private:
     WordTopicCountDist _wor2top;
     TopicCountDist _top_total;
-    WordDict _word_dict;
+    Word2IDDict _word2id_dict;
+    ID2WordDict _id2word_dict;
 };
 
 
-class Sampler {
+
+
+class LdaInfer {
 public:
-    Sampler(Model* model, double alpha, double beta) 
-    : _model(model), _alpha(alpha), _beta(beta) 
+    LdaInfer(string model_file, double alpha, double beta) 
+    : _model(model_file), _alpha(alpha), _beta(beta)
+    {    _num_topic = _model.GetTopicNum(); }
+
+    void Infer(const vector<string>& string_doc, int max_iter, vector<pair<string, int> >* word_topic)
     {
-        _num_topic = _model->GetTopicNum();
-        _num_vocal = _model->GetVocalNum();
+        Document doc;
+        InitTopicAssignment(string_doc, &doc);
+        for (int iter = 0; iter < max_iter; ++iter)
+        {
+            UpdateTopicForDocument(&doc);
+        }
+
+        Model::ID2WordDict* id2word_dict = _model.GetID2WordDict();
+        for (size_t i=0; i<doc._document.size(); ++i)
+        {
+            int word_id = doc._document[i];
+            int topic_id = doc._topic[i];
+            string word = (*id2word_dict)[word_id]; 
+            word_topic->push_back(pair<string, int>(word, topic_id));
+        }
     }
 
+private:
     void UpdateTopicForDocument(Document* doc)
     {
+        //cout<<"-------------------------------------------------------"<<endl;
+        //LOG(INFO)<<"Before Update Topic: "<<doc->document_output_string()<<endl;
+        //print_topic_count_dist(doc->_topic_dist);
+        //cout<<"-------------------------------------------------------"<<endl;
+
         int doc_size = doc->_document.size();
         for (size_t i = 0; i < doc_size; ++i)
         {
+            int old_topic_id = doc->_topic[i];
             TopicCountDist topic_count_dist;
             CalcTopicPosterior(i, doc, &topic_count_dist);
             int sampled_topic = SampleTopic(&topic_count_dist);
+            //LOG(INFO)<<"word "<<doc->_document[i]<<" sampled topic is "<<sampled_topic<<endl;
             // update topic assignment
             doc->_topic[i] = sampled_topic;
-            if (doc->_topic_dist.find(sampled_topic) == doc->_topic_dist.end()
-                || doc->_topic_dist[sampled_topic] == 0)
+            if (doc->_topic_dist.find(sampled_topic) == doc->_topic_dist.end())
             {
                 doc->_topic_dist[sampled_topic] = 1;
             }
             else {
-                doc->_topic_dist[sampled_topic] -= 1;
+                doc->_topic_dist[sampled_topic] += 1;
             }
+            doc->_topic_dist[old_topic_id] -= 1;
         }
     }
 
@@ -141,14 +207,14 @@ public:
     {
         int word_id = doc->_document[word_id_index];
         int old_topic_id = doc->_topic[word_id_index];
-        const TopicCountDist* word_topic_count_dist = _model->GetWordTopicCountDist(word_id);
+        const TopicCountDist* word_topic_count_dist = _model.GetWordTopicCountDist(word_id);
         for (TopicCountDist::const_iterator iter = word_topic_count_dist->begin();
              iter != word_topic_count_dist->end();
              ++iter)
         {
            int topic_id = iter->first;
            double topic_count = iter->second;
-           double topic_total_count = _model->GetTopicTotalCount(topic_id);
+           double topic_total_count = _model.GetTopicTotalCount(topic_id);
            double p_w_z = topic_count / topic_total_count;
 
            double adjust = topic_id == old_topic_id ? 1 : 0;
@@ -157,6 +223,9 @@ public:
            doc_topic_count -= adjust;
            
            (*topic_count_dist)[topic_id] = p_w_z * (doc_topic_count + _alpha);
+           //cout<<"+++posterior topic:"<<topic_id
+           //    <<" prob:"<<(*topic_count_dist)[topic_id]
+           //    <<" p_w_z:"<<p_w_z<<"  p_z:"<<(doc_topic_count + _alpha)<<endl;
         }
     }
 
@@ -164,7 +233,7 @@ public:
     {
         vector<TopicCountPair > topic_dist(topic_count_dist->begin(), topic_count_dist->end());
         size_t size = topic_dist.size();
-        for (int i = 1; i < size; ++i)
+        for (size_t i = 1; i < size; ++i)
             topic_dist[i].second += topic_dist[i-1].second;
         double rdm = rand() / static_cast<double>(RAND_MAX) * topic_dist[size-1].second;
         vector<TopicCountPair >::iterator iter = find_if(topic_dist.begin(), topic_dist.end(),
@@ -177,9 +246,9 @@ public:
         for (size_t i = 0; i < string_doc.size(); ++i)
         {
 
-            Model::WordDict* p_word_dict = _model->GetWordDict();
-            if (p_word_dict->find(string_doc[i]) == p_word_dict->end()) continue;
-            int word_id = (*p_word_dict)[string_doc[i]];
+            Model::Word2IDDict* p_word2id_dict = _model.GetWord2IDDict();
+            if (p_word2id_dict->find(string_doc[i]) == p_word2id_dict->end()) continue;
+            int word_id = (*p_word2id_dict)[string_doc[i]];
             doc->_document.push_back(word_id);
             int random_topic = static_cast<int>( rand() / static_cast<double>(RAND_MAX) * _num_topic);
             doc->_topic.push_back(random_topic);
@@ -190,34 +259,93 @@ public:
                 doc->_topic_dist[random_topic] ++;
         }
     }
+
 private:
-    Model* _model;
+    //Sampler _sampler; 
+    Model _model;
     int _num_topic;
-    int _num_vocal;
     double _alpha;
     double _beta;
 };
 
-
-class LdaInfer {
+class LDAQueryExtend {
 public:
-    LdaInfer(Sampler* sampler) : _sampler(sampler) { }
-
-    void Infer(const vector<string>& string_doc, int max_iter, Document* doc)
+    typedef pair<string, double> WordProb;
+    LDAQueryExtend(const string& model_file, double alpha, double beta)
+    : _infer(model_file, alpha, beta), _topic2word(6000)
     {
-        _sampler->InitTopicAssignment(string_doc, doc);
-        for (int iter = 0; iter < max_iter; ++iter)
+        ifstream ifs(model_file.c_str());
+        string buf;
+        int topic2word_size = _topic2word.size();
+        while (getline(ifs, buf))
         {
-            _sampler->UpdateTopicForDocument(doc);
+            if (buf.size() == 0 && buf[0] == '\n') continue;
+            istringstream ss(buf);
+            int topic_id;
+            ss >> topic_id;
+            if (topic2word_size <= topic_id) {
+                _topic2word.resize(topic2word_size);
+                topic2word_size = _topic2word.size();
+            }
+            
+            string item;
+            double total_count = 0.0;
+            while (ss >> item)
+            {
+                vector<string> tokens;
+                boost::split(tokens, item, boost::is_any_of(":"));
+                string word = tokens[0];
+                double count = boost::lexical_cast<double>(tokens[1]);
+                total_count += count;
+                _topic2word[topic_id].push_back(WordProb(word, count));
+            }
+            
+            for (size_t i = 0; i<_topic2word[topic_id].size(); ++i)
+                _topic2word[topic_id][i].second /= total_count;
+        }
+
+    }
+
+    void ExtendQuery(const vector<string>& tokens, unordered_map<string, double>* extended_query)
+    {
+        vector<pair<string, int> > word_topic;
+        _infer.Infer(tokens, 20, &word_topic);
+        int sz = word_topic.size();
+        TopicCountDist topic_dist;
+        for (size_t i=0; i<sz; ++i)
+        {
+            int topic_id = word_topic[i].second;
+            if (topic_dist.find(topic_id) == topic_dist.end())
+                topic_dist[topic_id] = 1.0/sz;
+            else
+                topic_dist[topic_id] += 1.0/sz;
+            cout<<word_topic[i].first<<":"<<word_topic[i].second<<endl;
+        }
+
+        for (TopicCountDist::iterator iter = topic_dist.begin();
+             iter != topic_dist.end();
+             ++iter)
+        {
+            int topic_id = iter->first;
+            double prob_topic = iter->second;
+            
+            vector<WordProb>& one_topic = _topic2word[topic_id];
+            int topic_word_size = one_topic.size();
+            for (size_t i=0; i<topic_word_size; ++i)
+            {
+                string word = one_topic[i].first;
+                double prob_topic2word = one_topic[i].second;
+                if (extended_query->find(word) == extended_query->end())
+                    (*extended_query)[word] = prob_topic * prob_topic2word;
+                else
+                    (*extended_query)[word] += prob_topic * prob_topic2word;
+            }
         }
     }
 
 private:
-    Sampler* _sampler; 
+    vector<vector<WordProb > > _topic2word; 
+    LdaInfer _infer;
 };
-
-
-
-
 
 #endif
